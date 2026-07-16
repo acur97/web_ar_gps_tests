9efb892d1fb30574b6b7ce955c0b302d3e42fc0c
Andres Camilo Urrego Restrepo <andres.urrego@polygon.us>
2026-07-15 19:40:53 -0500

    - base project - web build test

---
+++ Assets/_Scripts/TestLocation.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+
+public class TestLocation : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    public void IsEnabledByUser()
+    {
+        text.text = Input.location.isEnabledByUser.ToString();
+    }
+
+    public void LocationStart()
+    {
+        float desiredAccuracyInMeters = 10f;
+        float updateDistanceInMeters = 10f;
+
+        Input.location.Start(desiredAccuracyInMeters, updateDistanceInMeters);
+    }
+
+    public void CheckLocationStart()
+    {
+        text.text = Input.location.status.ToString();
+    }
+
+    public void CheckLocation()
+    {
+        text.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
+    }
+
+    public void LocationStop()
+    {
+        Input.location.Stop();
+    }
+}
