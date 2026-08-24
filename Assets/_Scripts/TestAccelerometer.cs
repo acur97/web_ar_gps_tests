604368778d0b62fa89bdf83ac069175d4b8fd2ef
Andres Camilo Urrego Restrepo <andres.urrego@polygon.us>
2026-08-24 17:37:05 -0500

    FromToRotation 3

--- Assets/_Scripts/TestAccelerometer.cs
+++ Assets/_Scripts/TestAccelerometer.cs
@@ -91,27 +91,27 @@ public class TestAccelerometer : MonoBehaviour
         text.text = "";
 
         // en pc sale error en todos
-        if (Accelerometer.current != null) // funciona en los dos, orientacion, vector3
+        if (Accelerometer.current != null) // funciona en los dos, gravedad + aceleracion, vector3
         {
             text.text += $"Accelerometer: {Accelerometer.current.acceleration.ReadValue()} \n";
         }
 
-        if (AttitudeSensor.current != null) // solo funciona en moderno, quaternio?, 
+        if (AttitudeSensor.current != null) // solo funciona en moderno, gyro, quaternio, 
         {
             text.text += $"AttitudeSensor: {AttitudeSensor.current.attitude.ReadValue()} \n";
         }
 
-        if (GravitySensor.current != null) // funciona en los dos, parece igual que Accelerometer, vector3
+        if (GravitySensor.current != null) // funciona en los dos, gravedad, vector3
         {
             text.text += $"GravitySensor: {GravitySensor.current.gravity.ReadValue()} \n";
         }
 
-        if (Gyroscope.current != null) // solo funciona en moderno, cantidad de giro, vector3
+        if (Gyroscope.current != null) // solo funciona en moderno, aceleracion del AttitudeSensor, vector3
         {
             text.text += $"Gyroscope: {Gyroscope.current.angularVelocity.ReadValue()} \n";
         }
 
-        if (LinearAccelerationSensor.current != null) // solo funciona en moderno, un suavizado de no se que, parece Gyroscope, vector3
+        if (LinearAccelerationSensor.current != null) // funciona en los dos, aceleracion, vector3
         {
             text.text += $"LinearAccelerationSensor: {LinearAccelerationSensor.current.acceleration.ReadValue()} \n";
         }
@@ -121,15 +121,15 @@ public class TestAccelerometer : MonoBehaviour
             text.text += $"MagneticFieldSensor: {MagneticFieldSensor.current.magneticField.ReadValue()}";
         }
 
-        if (Input.compass != null) // error en viejo, en nuevo nada, no muestra en ninguno de los dos
+        if (Input.compass != null) // vacio, vector3
         {
-            text.text += $"compass: {Input.compass.rawVector}";
+            text.text += $"compass: {Input.compass.rawVector} {Input.compass.magneticHeading} {Input.compass.trueHeading}";
         }
 
-        if (Accelerometer.current != null)
+        if (GravitySensor.current != null)
         {
             #region primera version funcional, al tener vertical el telefono se descontrola
-            //Vector3 value = Accelerometer.current.acceleration.ReadValue();
+            //Vector3 value = GravitySensor.current.gravity.ReadValue();
             //Vector3 value2 = new(-value.x, -value.y, value.z);
 
             //if (value2.sqrMagnitude > 0.01f)
@@ -139,35 +139,36 @@ public class TestAccelerometer : MonoBehaviour
             #endregion
 
             #region segunda version funcinal, pero se va desfazando
-            //Vector3 acceleration = Accelerometer.current.acceleration.ReadValue();
-            //Vector3 gravity = new(-acceleration.x, -acceleration.y, acceleration.z);
-
-            //if (gravity.sqrMagnitude > 0.01f)
-            //{
-            //    gravity.Normalize();
-
-            //    Quaternion tilt = Quaternion.FromToRotation(cube.up, gravity);
-            //    cube.rotation = tilt * cube.rotation;
-            //}
-            #endregion
-
-
-            Vector3 acceleration = Accelerometer.current.acceleration.ReadValue();
+            Vector3 acceleration = GravitySensor.current.gravity.ReadValue();
             Vector3 gravity = new(-acceleration.x, -acceleration.y, acceleration.z);
 
             if (gravity.sqrMagnitude > 0.01f)
             {
                 gravity.Normalize();
 
-                Vector3 forward = Vector3.ProjectOnPlane(cube.forward, gravity);
-
-                if (forward.sqrMagnitude > 0.001f)
-                {
-                    forward.Normalize();
-
-                    cube.rotation = Quaternion.LookRotation(forward, -gravity);
-                }
+                Quaternion tilt = Quaternion.FromToRotation(cube.up, gravity);
+                cube.rotation = tilt * Quaternion.identity /*cube.rotation*/;
             }
+            #endregion
+
+            #region
+            //Vector3 acceleration = GravitySensor.current.gravity.ReadValue();
+            //Vector3 gravity = new(-acceleration.x, -acceleration.y, acceleration.z);
+
+            //if (gravity.sqrMagnitude > 0.01f)
+            //{
+            //    gravity.Normalize();
+
+            //    Vector3 forward = Vector3.ProjectOnPlane(cube.forward, gravity);
+
+            //    if (forward.sqrMagnitude > 0.001f)
+            //    {
+            //        forward.Normalize();
+
+            //        cube.rotation = Quaternion.LookRotation(forward, -gravity);
+            //    }
+            //}
+            #endregion
         }
     }
 }
