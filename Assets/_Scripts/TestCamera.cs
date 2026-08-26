e382e0813ddf637f2c4031ef500d31bf868ab9ac
Andres Camilo Urrego Restrepo <andres.urrego@polygon.us>
2026-08-26 13:14:37 -0500

    - remove delays - order logs

--- Assets/_Scripts/TestCamera.cs
+++ Assets/_Scripts/TestCamera.cs
@@ -9,72 +9,68 @@ public class TestCamera : MonoBehaviour
     [SerializeField] private RawImage rImage;
     [SerializeField] private AspectRatioFitter aspectFitter;
 
-    [Space]
-    //[SerializeField] private WebCamTexture webCamTexture;
-    //[SerializeField] private int cameraIndex = -1;
-    //private WebCamDevice[] devices;
-
     private WebCamDevice frontCameraDevice;
     private WebCamDevice backCameraDevice;
     private WebCamDevice activeCameraDevice;
 
+    [Space]
     [SerializeField] private WebCamTexture frontCameraTexture;
     [SerializeField] private WebCamTexture backCameraTexture;
     [SerializeField] private WebCamTexture activeCameraTexture;
 
     private CancellationTokenSource token;
 
-    //public void CameraIndex(string index)
-    //{
-    //    if (!string.IsNullOrEmpty(index))
-    //        cameraIndex = int.Parse(index);
-    //}
-
     public void StartCamera()
     {
         StartAwaitCamera().Forget();
     }
 
-    //private string DescribeResolution(Resolution res)
-    //{
-    //    return $"{res.width}x{res.height}@{res.refreshRateRatio.value}Hz";
-    //}
+    private string DescribeResolution(Resolution res)
+    {
+        return $"{res.width}x{res.height}@{res.refreshRateRatio.value}Hz";
+    }
 
-    //private string DescribeResolutions(WebCamDevice dev)
-    //{
-    //    return string.Join(", ", dev.availableResolutions.Select(res => DescribeResolution(res)));
-    //}
+    private string DescribeResolutions(WebCamDevice dev)
+    {
+        return string.Join(", ", dev.availableResolutions.Select(res => DescribeResolution(res)));
+    }
 
     public async UniTaskVoid StartAwaitCamera()
     {
-        Debug.Log("StartAwaitCamera");
         token?.Cancel();
         token = new CancellationTokenSource();
-
-        Debug.Log("Find webcams before RequestUserAuthorization");
-        foreach (WebCamDevice device in WebCamTexture.devices)
-        {
-            Debug.Log("Name: " + device.name);
-        }
 
         rImage.enabled = true;
         Debug.Log("RequestUserAuthorization");
         await Application.RequestUserAuthorization(UserAuthorization.WebCam);
 
-        Debug.Log("WaitUntil HasUserAuthorization");
-        await UniTask.WaitUntil(() => Application.HasUserAuthorization(UserAuthorization.WebCam), cancellationToken: token.Token);
+        if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            Debug.LogWarning("Authorization error");
+            return;
+        }
 
-        Debug.Log("WaitUntil NextFrame");
-        await UniTask.NextFrame(token.Token);
-
-        //WebCamTexture.allowThreadedTextureCreation = false;
-
-        Debug.Log("WaitUntil WebCamTexture.devices");
         await UniTask.WaitUntil(() => WebCamTexture.devices.Length > 0, cancellationToken: token.Token);
-        Debug.Log("WaitUntil NextFrame");
-        await UniTask.NextFrame(token.Token);
 
-        Debug.Log("devices.First and Last");
+        Debug.Log("devices:");
+        foreach (WebCamDevice device in WebCamTexture.devices)
+        {
+            string desc = $"name: {device.name}. type: {device.kind}. ";
+
+            if (device.depthCameraName != null)
+                desc += $"Depth support: ({device.depthCameraName}). ";
+
+            desc += $"Direction: {(device.isFrontFacing ? "Front" : "Rear")}. ";
+
+            if (device.isAutoFocusPointSupported)
+                desc += "Auto focus support. ";
+
+            if (device.availableResolutions != null)
+                desc += $"Supported resolutions: {DescribeResolutions(device)}. ";
+
+            Debug.LogWarning(desc);
+        }
+
         frontCameraDevice = WebCamTexture.devices.First();
         backCameraDevice = WebCamTexture.devices.Last();
 
@@ -84,34 +80,10 @@ public class TestCamera : MonoBehaviour
         backCameraTexture = new WebCamTexture(backCameraDevice.name);
 
         SetActiveCamera(backCameraTexture);
-
-        //foreach (WebCamDevice device in devices)
-        //{
-        //    string desc = $"name: {device.name}. type: {device.kind}. ";
-
-        //    if (device.depthCameraName != null)
-        //        desc += $"Depth support: ({device.depthCameraName}). ";
-
-        //    desc += $"Direction: {(device.isFrontFacing ? "Front" : "Rear")}. ";
-
-        //    if (device.isAutoFocusPointSupported)
-        //        desc += "Auto focus support. ";
-
-        //    if (device.availableResolutions != null)
-        //        desc += $"Supported resolutions: {DescribeResolutions(device)}. ";
-
-        //    Debug.LogWarning(desc);
-        //}
-
-        //bool vflip = webCamTexture.videoVerticallyMirrored;
-        //Vector2 scale = new(1, vflip ? -1 : 1);
-        //Vector2 offset = new(0, vflip ? 1 : 0);
     }
 
     private void SetActiveCamera(WebCamTexture cameraToUse)
     {
-        Debug.Log($"SetActiveCamera {cameraToUse.name}");
-
         if (activeCameraTexture != null)
             activeCameraTexture.Stop();
 
@@ -171,10 +143,14 @@ public class TestCamera : MonoBehaviour
         //image.rectTransform.localEulerAngles = rotationVector;
 
         Debug.LogWarning(
-            $"resolution: {activeCameraTexture.width}x{activeCameraTexture.height} {activeCameraTexture.didUpdateThisFrame} {activeCameraTexture.isPlaying}."); // 480x640
+            $"currentResolution:{activeCameraTexture.width}x{activeCameraTexture.height} | UpdateThisFrame:{activeCameraTexture.didUpdateThisFrame} | isPlaying:{activeCameraTexture.isPlaying}"); // 480x640
 
         // Set AspectRatioFitter's ratio
-        aspectFitter.aspectRatio = activeCameraTexture.width / (float)activeCameraTexture.height; ;
+        aspectFitter.aspectRatio = activeCameraTexture.width / (float)activeCameraTexture.height;
+
+        //bool vflip = webCamTexture.videoVerticallyMirrored;
+        //Vector2 scale = new(1, vflip ? -1 : 1);
+        //Vector2 offset = new(0, vflip ? 1 : 0);
 
         // Unflip if vertically flipped
         //image.uvRect =
