916ea6e3ba9bb55b83439514ed64b25e3df50e0e
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2026-09-23 16:57:25 -0500

    test fps when camera starts

--- Assets/_Scripts/Systems/Camera/CameraManager.cs
+++ Assets/_Scripts/Systems/Camera/CameraManager.cs
@@ -48,19 +48,17 @@ public class CameraManager : MonoBehaviour
             return;
         }
 
-        if (FPSCounter.fps < 55)
-        {
-            await UniTask.WaitForSeconds(1);
-        }
-        else if (FPSCounter.fps <= 45)
+        Debug.Log($"current fps: {FPSCounter.fps}");
+        // delay for slow devices
+        if (FPSCounter.fps < 55 && FPSCounter.fps >= 45)
         {
             Debug.Log("2 second delay");
-            await UniTask.WaitForSeconds(2); // delay for slow devices
+            await UniTask.WaitForSeconds(2);
         }
-        else if (FPSCounter.fps <= 30)
+        else if (FPSCounter.fps < 45)
         {
             Debug.Log("4 second delay");
-            await UniTask.WaitForSeconds(4); // delay for slow devices
+            await UniTask.WaitForSeconds(4);
         }
 
         await UniTask.WaitUntil(() => WebCamTexture.devices.Length > 0);
